bb2ea5f4fb6ab09f26081c91badac5ad0469e66e
Jose Sandino <josesandinonio@gmail.com>
2018-10-22 20:55:17 -0600

    0.0.01
    0.0.01

--- templates/header.php
+++ templates/header.php
@@ -3,7 +3,7 @@
 <head>
   <meta http-equiv="Content-type" content="text/html; charset=utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <title>AdminLTE 2 | Blank Page</title>
+  <title>VisitMed</title>
   <!-- Tell the browser to be responsive to screen width -->
   <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
   <!-- Bootstrap 3.3.7 -->
