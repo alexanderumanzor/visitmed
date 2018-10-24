62fd8ccb889968c0ab3d102f10905a906c65f4df
josesandino <josesandinonio@gmail.com>
2018-10-24 16:50:50 -0600

    Version 0.0.01
    Version 0.0.01

--- funciones/bd_conexion.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-    $conn = new mysqli('localhost', 'root', 'root', 'visitmed');
-
-    if($conn->connect_error) {
-        echo $error -> $conn->connect_error;
-    }
-   mysqli_query("SET NAMES'utf8'");
-?>
--- funciones/funciones.php
+++
@@ -1,3 +0,0 @@
- <?php
-    
-    include_once 'bd_conexion.php';
